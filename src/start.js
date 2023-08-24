d75f7d1bf0910e50f21bd7781491804f7dc20512
jeffery959 <aaronjeffery529@gmail.com>
2023-08-23 21:59:15 -0400

    Ready to Deploy

---
+++ src/start.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import UserRouter from '../router/UserRouter.js';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv'
+import colors from 'colors';
+import cors from 'cors'
+dotenv.config()
+const app=express()
+const Port = process.env.PORT
+const MongoDB_URI = process.env.MONGODB_URI
+
+//Middleware
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+//dbConfig
+mongoose.connect(MongoDB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>{console.log('Mogodb database connected'.underline.blue)})
+.catch((err)=>{console.log('Error connecting to MongoDB:',err.message)})
+
+
+//Routing
+app.use('/router',UserRouter)
+
+//Middleware
+app.listen(Port,()=>console.log("connected to http://localhost:"+Port))
